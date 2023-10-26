7192e145fb8c4b377f5dc9cfeccee22298da9ea7
Endy B <endyb.dev@gmail.com>
2023-10-26 16:38:27 +1100

    Logging, preconfig

---
+++ app/utils/loggin_gen.py
@@ -0,0 +1,9 @@
+import logging
+
+logging.basicConfig(
+    level=logging.DEBUG,
+    format="%(asctime)s %(levelname)s %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
+    filename="tradeai_query.log"
+)
+logging.info("""user 1 - question: this is a question | SQL: SELECT * FROM fake_table | AI_observation: This is the AI Opinion about the query""")
